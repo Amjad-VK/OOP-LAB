b3e430e22b8a02317921e703ca5fa99090b18f7f
Amjad-VK <74619521+Amjad-VK@users.noreply.github.com>
2021-05-04 22:46:15 +0530

    Add files via upload

---
+++ Multivariables.java
@@ -0,0 +1,39 @@
+class Multivariables{
+ public static void main(String args[]){
+int a,b,addn,mul,div,diff;
+a=5;
+b=3;
+
+System.out.println("The 1st no is:" + a);
+System.out.println("The 2nd no is:" + b);
+
+addn=a+b;
+mul=a*b;
+div=a/b;
+diff=a-b;
+
+System.out.println("Addition:" + addn);
+
+System.out.println("Multiplication:" + mul);
+
+
+System.out.println("Division:" + div);
+
+
+System.out.println("Subtraction:" + diff);
+
+}
+}
+
+
+------------Output---------------
+
+D:\ONLINE CLASS\S2\OOP>javac Multivariables.java
+
+D:\ONLINE CLASS\S2\OOP>java Multivariables
+The 1st no is:5
+The 2nd no is:3
+Addition:8
+Multiplication:15
+Division:1
+Subtraction:2
