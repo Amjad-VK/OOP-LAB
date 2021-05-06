2ab9538ee7de33fe1a4d1ade29196d86b5807dc7
Amjad-VK <74619521+Amjad-VK@users.noreply.github.com>
2021-05-07 01:33:45 +0530

    Add files via upload

---
+++ Exercise 1/Float.java
@@ -0,0 +1,44 @@
+/*Q) Write a Java program to read a floating-point number and if the number is zero it prints "zero", otherwise, print "positive" or "negative". Add "small" if the
+     absolute value of the number is less than 1, or "large" if it exceeds 1,00,000. */
+
+import java.util.Scanner;
+class Float{
+public static void main(String args[]){
+float num;
+System.out.println("Enter a Number:");
+Scanner no=new Scanner(System.in);
+num=no.nextFloat();
+if(num==0)
+{
+System.out.println("The number is Zero");
+}
+else if(num>0)
+{
+System.out.println("The number is Positive");
+}
+else if(num<0)
+{
+    System.out.println("The number is negative");
+}
+else if(num>100000)
+   {
+     System.out.println("The number is large");
+   }
+else if(num<1)
+   {
+     System.out.println("The number is small"); 
+   }
+
+else
+{
+System.out.println("The number is Negative");
+}
+}
+}
+
+
+/*-----OUTPUT----------
+Enter a Number:
+10
+The number is Positive
+*/
