ca09793987f533b72b2e8b4211d94cf4ba96e7a5
Amjad-VK <74619521+Amjad-VK@users.noreply.github.com>
2021-08-16 11:34:21 -0700

    Add files via upload

---
+++ Exercise 7/FibboEven.java
@@ -0,0 +1,108 @@
+//2.Define 2 classes; one for generating Fibonacci numbers and other for displaying even numbers in a given range. Implement using threads. (Runnable Interface)
+import java.util.Scanner;
+
+class FibonacciThread implements Runnable{
+
+    public void run(){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter No of Numbers for Fibonacci series :");
+        int limit = sc.nextInt();
+  
+       try{
+   
+            int n1=0,n2=1,n3,i;    
+            System.out.println("FIBONACCI : "+n1);   //printing 0 and 1    
+            System.out.println("FIBONACCI : "+n2);
+              for(i=2;i<limit;++i)//loop starts from 2 because 0 and 1 are already printed    
+              {    
+                   n3=n1+n2;    
+                   System.out.println("FIBONACCI : "+n3);    
+                   n1=n2;    
+                   n2=n3;    
+                   Thread.sleep(250); 
+              }    
+        
+     }catch(InterruptedException e)
+     {
+            System.out.println("FIBBONACCI INTERRUPTED :"+e);       
+     } 
+     System.out.println("FIBBONACCI THREAD FINISHED..!");          
+    }          
+
+}
+
+
+        
+
+
+
+
+class EvennumbersThread implements Runnable{
+ 
+ public void run(){
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter No of Numbers for Even numbers : ");
+    int limit = sc.nextInt();
+  
+   try{
+        
+        for(int i =1;i<=limit;i++)
+        {   
+            if(i%2 ==0){
+                System.out.println("EVEN NO   : "+i);
+                Thread.sleep(250);
+            }
+        }
+        
+     }catch(InterruptedException e)
+     {
+            System.out.println("EVEN NO INTERRUPTED :"+e);       
+     } 
+   System.out.println("EVEN THREAD FINISHED..!");          
+ }          
+
+}
+
+
+
+class FibboEven 
+{
+    public static void main(String args[])
+    {        
+            EvennumbersThread EN = new EvennumbersThread(); // instance of implemented class created
+            Thread EvenThread = new Thread(EN);             // thread created    
+            
+            FibonacciThread FN = new FibonacciThread();  // instance of implemented class created
+            Thread FibboThread = new Thread(FN);// thread created    
+           
+            //starting threads 
+            EvenThread.start();  
+            FibboThread.start();                   
+    }
+}
+
+
+/*
+OUTPUT
+------
+E:\ONLINE CLASS\S2\OOP\Exercises\Exercise 7\TTTT>java FibboEven
+Enter No of Numbers for Fibonacci series :Enter No of Numbers for Even numbers : 5
+6
+FIBONACCI : 0
+EVEN NO   : 2
+FIBONACCI : 1
+FIBONACCI : 1
+EVEN NO   : 4
+FIBONACCI : 2
+EVEN THREAD FINISHED..!
+FIBONACCI : 3
+FIBONACCI : 5
+FIBBONACCI THREAD FINISHED..!
+
+*/
+
+
